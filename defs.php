f2e4b9e2f76761613e518ba0aaddcc865f5cd314
ackroo <proddev.account@ackroo.com>
2013-12-11 09:51:02 -0500

    Create defs.php

---
+++ defs.php
@@ -0,0 +1,11 @@
+<?php
+
+$api = "https://api.ackroo.net";
+$oauth_provider = "https://manage.ackroo.com";
+$dev_portal = "http://developer.ackroo.com";
+$redirect_uri = "http://localhost:8000/api_client";
+$app_id = "e42a433c2fa667e9c143437f87df889c2e2161588d29358b561bb6aee750e737";
+$secret = "aba7820f1fe610c30241eb3be0cfdbece117423b9fb836183349b67e6178fe6e";
+$scopes = "card";
+
+?>
